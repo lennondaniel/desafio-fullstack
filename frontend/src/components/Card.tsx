3480e66f1ee65c0f398762b5cfac17c4e86b7b66
Lennon Daniel <lennon.v90@gmail.com>
2024-05-08 12:48:10 -0300

    feat: add component card

---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,38 @@
+import { Task } from "@/services/tasks.service";
+import Checkbox from "./Checkbox";
+import ButtonActions from "./ButtonActions";
+import { RiMenuSearchLine } from "react-icons/ri";
+import { FaPencilAlt, FaRegTrashAlt } from "react-icons/fa";
+import Skeleton from "react-loading-skeleton";
+
+interface Props {
+    task: Task
+
+}
+
+export default function Card ({task}: Props) {
+    return (
+        <div className="flex justify-between rounded bg-white items-center p-3 mb-5 shadow-sm">
+            <div className="flex gap-3">
+                {<Checkbox checked={task.completed ?? false }/> || <Skeleton />}
+                <p className={`capitalize text-base font-regular text-gray-500 ${task.completed && 'line-through'}`}>{task.description || <Skeleton />}</p>
+            </div>
+            {
+                !task.completed &&
+                    <div className="flex gap-3">
+                        <ButtonActions>
+                            <RiMenuSearchLine />
+                        </ButtonActions>
+                        <ButtonActions>
+                            <FaPencilAlt />
+                        </ButtonActions>
+                        <ButtonActions>
+                            <FaRegTrashAlt />
+                        </ButtonActions>
+                    </div>
+            }
+
+
+        </div>
+    )
+}
